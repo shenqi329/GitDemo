10c9ce9ab98775fbef811a9db0a81718360da4e0
shenqi329 <shenqi329@shenqi329deMac.local>
2014-09-16 21:17:14 +0800

    Add TestClass

---
+++ GitDemo/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GitDemo
+//
+//  Created by shenqi329 on 14-9-16.
+//  Copyright (c) 2014年 shenqi329. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ GitDemo/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  GitDemo
+//
+//  Created by shenqi329 on 14-9-16.
+//  Copyright (c) 2014年 shenqi329. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
